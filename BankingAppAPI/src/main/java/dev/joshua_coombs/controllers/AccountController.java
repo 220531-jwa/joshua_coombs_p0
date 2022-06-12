48130b8ffaa28d3d5f0991502b8eb2b44ef78f7d
joshuacoombs <h0wlpenwyrm@gmail.com>
2022-06-12 16:11:15 -0700

    adding updates to unfinished Project 0

--- BankingAppAPI/src/main/java/dev/joshua_coombs/controllers/AccountController.java
+++ BankingAppAPI/src/main/java/dev/joshua_coombs/controllers/AccountController.java
@@ -1,13 +1,17 @@
 package dev.joshua_coombs.controllers;
 
-import dev.joshua_coombs.services.ClientService;
+import dev.joshua_coombs.models.Account;
+import dev.joshua_coombs.services.AccountService;
 import io.javalin.http.Context;
 
 public class AccountController {
-	private static ClientService clientService = new ClientService();
+	private static AccountService accountService = new AccountService();
 	
 	public static void createAccount(Context ctx) {
-		
+		ctx.status(201);
+		Account accountFromRequestBody = ctx.bodyAsClass(Account.class);
+		Account a = accountService.createAccount(accountFromRequestBody);
+		ctx.json(a);
 	}
 	
 	public static void getAllAccountsByClientId(Context ctx) {
@@ -19,9 +23,13 @@ public class AccountController {
 	}
 	
 	public static void updateAccount(Context ctx) {
-		
+		Account changeAccount = ctx.bodyAsClass(Account.class);
+		System.out.println("changeAccount -= " + changeAccount);
+		accountService.updateAccount(changeAccount);
 	}
 	public static void deleteAccount(Context ctx) {
-		
+		int accountNumber = Integer.parseInt(ctx.pathParam("account_number"));
+		int clientId = Integer.parseInt(ctx.pathParam("client_id"));
+		accountService.deleteAccount(accountNumber, clientId);
 	}
 }
