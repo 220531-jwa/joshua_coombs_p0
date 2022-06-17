0910c38b60ffb4cb2147c728769bc8e4856508e0
joshuacoombs <h0wlpenwyrm@gmail.com>
2022-06-17 02:11:16 -0700

    adding updates instead of sleeping

--- BankingAppAPI/src/test/java/dev/joshua_coombs/services/AccountServiceTest.java
+++ BankingAppAPI/src/test/java/dev/joshua_coombs/services/AccountServiceTest.java
@@ -1,6 +1,10 @@
 package dev.joshua_coombs.services;
 
-import static org.mockito.Mockito.mock;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import org.junit.jupiter.api.*;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -8,6 +12,9 @@ import org.mockito.junit.jupiter.*;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 
+import dev.joshua_coombs.models.Account;
+import dev.joshua_coombs.models.AlternateCALeftJoin;
+import dev.joshua_coombs.models.ClientAccountLeftJoin;
 import dev.joshua_coombs.repositories.AccountDAO;
 
 @ExtendWith(MockitoExtension.class)
@@ -19,91 +26,88 @@ public class AccountServiceTest {
 	
 	@Test
 	public static void createAccountPositiveTest() {
-		
-	}
-	
-	@Test
-	public static void createAccounNegativetTest() {
-		
+		Account mockAccount = new Account(1, 7, 500, 500);
+		assertEquals(when(mockAccountDao.createAccount(mockAccount)).thenReturn(mockAccount), 
+				accountService.createAccount(mockAccount));
 	}
 	
 	@Test
 	public static void getAllAccountsByClientIdPositiveTest() {
-		
+		List<ClientAccountLeftJoin> mockCA = new ArrayList<>();
+		mockCA.add(new ClientAccountLeftJoin(1, "kurt", "cobain", 1, 200000, 500000));
+		mockCA.add(new ClientAccountLeftJoin(1, "kurt", "cobain", 2, 10000, 2000));
+		mockCA.add(new ClientAccountLeftJoin(1, "kurt", "cobain", 3, 120000, 4000));
+		assertEquals(when(mockAccountDao.getAllAccountsByClientId(1)).thenReturn(mockCA), 
+				accountService.getAllAccountsByClientId(1));
 	}
 	
 	@Test
-	public static void getAllAccountsByClientIdNegativeTest() {
-		
+	public static void getSpecificAccountByClientIdPositiveTest() throws Exception {
+		int clientId = 2;
+		int accountNumber = 2;
+		ClientAccountLeftJoin ca = new ClientAccountLeftJoin(
+				clientId, "amahl", "farouk", accountNumber, 20, 20);
+		assertEquals(when(mockAccountDao.getSpecificAccountByClientId(clientId, accountNumber)).thenReturn(ca), 
+				accountService.getSpecificAccountByClientId(clientId, accountNumber));
 	}
 	
 	@Test
-	public static void getSpecificAccountByClientIdPositiveTest() {
-		
-	}
-	
-	@Test
-	public static void getSpecificAccountByClientIdNegativeTest() {
-		
-	}
-	
-	@Test
-	public static void getAccountsInValueRangePositiveTest() {
-		
-	}
-	
-	@Test
-	public static void getAccountsInValueRangeNegativeTest() {
-		
+	public static void getAccountsInValueRangeCheckingPositiveTest() {
+		int clientId = 2;
+		String whichType = "checking";
+		int low = 400;
+		int high = 2000;
+		assertEquals(when(mockAccountDao.getAccountsInValueRange(clientId, whichType, low, high)), 
+				accountService.getAccountsInValueRange(clientId, whichType, low, high));
 	}
 	
 	@Test
 	public static void updateAccountPositiveTest() {
-		
-	}
-	
-	@Test
-	public static void updateAccountNegativeTest() {
-		
+		int clientId = 1;
+		int accountNumber = 1;
+		int checkingAmount = 455;
+		int savingsAmount = 554;
+		assertEquals(when(mockAccountDao.updateAccount(clientId, accountNumber, checkingAmount, savingsAmount)), 
+				accountService.updateAccount(clientId, accountNumber, checkingAmount, savingsAmount));
 	}
 	
 	@Test
 	public static void withdrawPositiveTest() {
-		
-	}
-	
-	@Test
-	public static void withdrawNegativeTest() {
-		
+		int clientId = 1; 
+		int accountNumber = 1;
+		String whichType = "checking";
+		int amountToWithdraw = 5;
+		assertEquals(when(mockAccountDao.withdraw(clientId, accountNumber, whichType, amountToWithdraw)), 
+				accountService.withdraw(clientId, accountNumber, whichType, amountToWithdraw));
 	}
 	
 	@Test
 	public static void depositPositiveTest() {
-		
-	}
-	
-	@Test
-	public static void depositNegativeTest() {
-		
+		int clientId = 1; 
+		int accountNumber = 1;
+		String whichType = "checking";
+		int amountToDeposit = 5;
+		assertEquals(when(mockAccountDao.withdraw(clientId, accountNumber, whichType, amountToDeposit)), 
+				accountService.withdraw(clientId, accountNumber, whichType, amountToDeposit));
 	}
 	
 	@Test
 	public static void transferPositiveTest() {
-		
-	}
-	
-	@Test
-	public static void transferNegativeTest() {
-		
+		int clientId = 1;
+		int fromAccount = 1;
+		int toAccount = 2;
+		String fromWhichType = "checking";
+		String toWhichType = "checking";
+		int amount = 50;
+		assertEquals(when(mockAccountDao.transfer(clientId, fromAccount, toAccount, fromWhichType, toWhichType, amount)), 
+				accountService.transfer(clientId, fromAccount, toAccount, fromWhichType, toWhichType, amount));
 	}
 	
 	@Test
 	public static void deleteAccountPositiveTest() {
-		
-	}
-	
-	@Test
-	public static void deleteAccountNegativeTest() {
-		
+		int clientId = 1;
+		int accountNumber = 1;
+		assertEquals(when(mockAccountDao.deleteAccount(clientId, accountNumber)), 
+				accountService.deleteAccount(clientId, accountNumber));
 	}
 }
--- BankingAppAPI/src/test/java/dev/joshua_coombs/services/ClientServiceTest.java
+++ BankingAppAPI/src/test/java/dev/joshua_coombs/services/ClientServiceTest.java
@@ -23,17 +23,11 @@ public class ClientServiceTest {
 	@Mock
 	private static ClientDAO mockClientDao;
 	
-	public static class Exception404 extends Exception {
-		public Exception404(String errorMessage) {
-			super(errorMessage);
-		}
-	}
-	
 	@Test
 	private static void createClientPositiveTest() {
 		Client mockClient = new Client(1, "arbitrary", "immaterial");
-		when(mockClientDao.createClient(mockClient)).thenReturn(mockClient);
-		assertEquals(mockClient, clientService.createClient(mockClient));
+		assertEquals(when(mockClientDao.createClient(mockClient)).thenReturn(mockClient), 
+				clientService.createClient(mockClient));
 	}
 	
 	@Test
@@ -43,23 +37,15 @@ public class ClientServiceTest {
 		mockClients.add(new Client(2, "ghi", "jkl"));
 		mockClients.add(new Client(3, "mno", "pqr"));
 		mockClients.add(new Client(4, "stu", "vwxyz"));
-		when(mockClientDao.getAllClients()).thenReturn(mockClients);
-		assertEquals(mockClients, clientService.getAllClients());
+		assertEquals(when(mockClientDao.getAllClients()).thenReturn(mockClients), 
+				clientService.getAllClients());
 	}
 	
 	@Test
 	private static void getClientByIdPositiveTest() throws Exception {
 		Client mockClient = new Client(10, "arbitrary", "immaterial");
-		when(mockClientDao.getClientById(10)).thenReturn(mockClient);
-		assertEquals(mockClient, clientService.getClientById(10));
-	}
-	
-	@Test
-	private static void getClientByIdNegativeTest() {
-		Client mockClient = null;
-		Exception404 e = new Exception404("Not found");
 		assertEquals(when(mockClientDao.getClientById(10)).thenReturn(mockClient), 
-				e.getMessage());
+				clientService.getClientById(10));
 	}
 	
 	@Test
@@ -70,22 +56,9 @@ public class ClientServiceTest {
 	}
 	
 	@Test
-	private static void updateClientNegativeTest() {
-		int id = 197;
-		assertEquals(when(mockClientDao.updateClientById(id)).thenReturn(false), clientService.updateClient(id));
-	}
-	
-	@Test
 	private static void deleteClientByIdPositiveTest() {
 		Client mockClient = new Client(15, "arbitrary", "immaterial");
 		assertEquals(when(mockClientDao.deleteClientById(mockClient.getId())).thenReturn(true), 
 				clientService.deleteClientById(15));
-	}
-	
-	@Test
-	private static void deleteClientByIdNegativeTest() {
-		int id = 1440;
-		assertEquals(when(mockClientDao.deleteClientById(id)).thenReturn(false), 
-				clientService.deleteClientById(1440));
 	}
 }
